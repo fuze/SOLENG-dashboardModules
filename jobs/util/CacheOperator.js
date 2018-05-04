ae33449bf25dc11664b22cc03bd8219c91d28b7d
ifrias-fuze <ifrias@fuze.com>
2018-05-04 17:09:07 +0100

    We need a cache operator to query and insert data for specific cases ( user data, peer data )

---
+++ jobs/util/CacheOperator.js
@@ -0,0 +1,34 @@
+const cacheProp = Symbol('cache');
+const { InMemoryCache } = require('./auth/InMemoryCache');
+const { GenericCacheEntry } = require('./synapse/GenericCacheEntry');
+
+class CacheOperator {
+  constructor(cache) {
+    this[cacheProp] = cache;
+  }
+
+  getUserToken(user) {
+    return this[cacheProp].get(`user_${user}`);
+  }
+
+  setUserToken(user, value) {
+    this[cacheProp].set(`user_${user}`, value);
+  }
+
+  getPeerInfo(peer) {
+    return this[cacheProp].get(`peer_${peer}`).value;
+  }
+
+  setPeerInfo(peer, value) {
+    const entry = new GenericCacheEntry(value);
+    this[cacheProp].set(`peer_${peer}`, entry);
+  }
+
+  static create() {
+    return new CacheOperator(InMemoryCache.instance);
+  }
+}
+
+module.exports = {
+  CacheOperator,
+}
