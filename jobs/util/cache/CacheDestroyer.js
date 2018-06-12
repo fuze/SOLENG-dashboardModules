2289e10e5bda8fe0f6fdfb29bed5015bbda63e35
ifrias-fuze <ifrias@fuze.com>
2018-06-12 11:31:15 +0100

    use the map on the CacheDestroyer class

--- jobs/util/cache/CacheDestroyer.js
+++ jobs/util/cache/CacheDestroyer.js
@@ -1,5 +1,6 @@
 const _cache = Symbol('cache');
 const _gatherKeysToDelete = Symbol('gatherKeysToDel');
+const _map = Symbol('map');
 
 // const purgeExecutionInterval = 1000 * 60 * 60;
 const purgeExecutionInterval = 1000 * 60;
@@ -8,10 +9,13 @@ class CacheDestroyer {
   constructor(cache) {
     this[_cache] = cache;
 
+    this[_map] = this[_cache].getAll();
+
     this[_gatherKeysToDelete] = function() {
       const toDelKeys = [];
-      for (let [key] of this[_cache]) {
-        if (!this[_cache].get(key).stillValid()) {
+      
+      for (let [key] of this[_map]) {
+        if (!map.get(key).stillValid()) {
           toDelKeys.push(key);
         }
       }
@@ -26,7 +30,7 @@ class CacheDestroyer {
     console.log('purging...');
     const toDelKeys = this[_gatherKeysToDelete]();
     console.log(toDelKeys);
-    toDelKeys.forEach(keyToDel => this[_cache].delete(keyToDel));
+    toDelKeys.forEach(keyToDel => this[_map].delete(keyToDel));
   }
 }
 
